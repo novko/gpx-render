eef2c39cb5273d51e4cce4b79139a3426f2e0344
novko <martin.novacik@gmail.com>
2014-10-06 17:18:06 +0200

    Html popup test

---
+++ ExtensionHtml/popup.js
@@ -0,0 +1,4 @@
+alert("test alert");
+alert("malik");
+chrome.downloads.download({ "url": "http://www.strava.com/activities/203786353/export_gpx" }, function () { alert("stiahnute"); });
+alert("done all");
